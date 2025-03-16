6666f2cd8c22665a7c05fcedd13d5b9a27863832
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-03-16 16:12:00 +0700

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>Lỗi 404</div>,
+
+    path: "/dangnhap",
+    element: <DangNhap />,
+    children: [
+      {
+        path: "/about",
+        element: <about />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
