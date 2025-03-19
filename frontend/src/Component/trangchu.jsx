20f6b42871d468330a06e15e2fd3f04bb52bbff2
Kienbmt2005 <kien102201@donga.edu.vn>
2025-03-19 09:12:32 +0700

    oke

---
+++ frontend/src/Component/trangchu.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './trangchu.css';
+
+function Trangchu() {
+    return (
+        <div className='trangchu'>
+            <div id="trangchu">
+                <div className="breaking-news">
+                    <a href="">Gaza truce shattered </a>l
+                    <a href="">Italian dollar homes </a>l
+                    <a href="">Toronto plane crash survivor </a>l
+                    <a href="">Eva Longoria </a>l
+                    <a href="">Lebanon-Syria ceasefire </a>l
+                    <a href="">Hong Kong tycoon dies at 97 </a>l
+                    <a href="">Lady Gaga</a>
+                </div>
+                <div className="news">
+                    <div className="slide-bar-left">đă</div>
+                    <div className="slide-bar-between">đă</div>
+                    <div className="slide-bar-right">ưda</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Trangchu;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,25 +4,32 @@ import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Trangchu from "./Component/trangchu.jsx"
+import Header from "./Component/header.jsx"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <div>Lỗi 404</div>,
-
-    path: "/dangnhap",
-    element: <DangNhap />,
     children: [
       {
-        path: "/about",
-        element: <about />,
+        path: "/",
+        element: <Navigate to="/trangchu" replace />, 
       },
+      {
+        path:"/header",
+        element: <Header/>,
+      },
+      {
+        path:"/trangchu",
+        element: <Trangchu/>,
+      }
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(  
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
