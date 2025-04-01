2ecf5875033ca22f06c2eeba221138893f7c2cca
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-04-01 16:19:57 +0700

    fix css

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -7,16 +7,16 @@ function Header(){
             <div className="list-menu">
                 <div className="list-tilte">
                     <a href=""><img src="https://icons.iconarchive.com/icons/colebemis/feather/48/list-icon.png" alt="" /></a>
-                    <a href=""><img src="" alt="" /></a>
-                    <a href="">US</a>
-                    <a href="">World</a>
-                    <a href="">Politics</a>
-                    <a href="">Business</a>
-                    <a href="">Health</a>
-                    <a href="">Entertainment</a>
-                    <a href="">Style</a>
-                    <a href="">Travel</a>
-                    <a href="">Sports</a>
+                    <a href="/"><img src="" alt="" /></a>
+                    <a href="/us">US</a>
+                    <a href="/world">World</a>
+                    <a href="/politics">Politics</a>
+                    <a href="/business">Business</a>
+                    <a href="/health">Health</a>
+                    <a href="/entertainment">Entertainment</a>
+                    <a href="style">Style</a>
+                    <a href="/travel">Travel</a>
+                    <a href="sports">Sports</a>
                     <a href="">
                         <div className="more">More
                             <img src="https://icons.iconarchive.com/icons/icons8/ios7/48/Arrows-Down-4-icon.png" alt="" />
@@ -35,21 +35,21 @@ function Header(){
                 <div className="login">
                     <div id="login">
                         <img src="https://icons.iconarchive.com/icons/iconsmind/outline/48/Video-5-icon.png" alt="" />
-                        <a href="">Watch</a>
+                        <a href="/watch">Watch</a>
                     </div>
                     <div id="login">
                         <img src="https://icons.iconarchive.com/icons/iconsmind/outline/48/Headphones-icon.png" alt="" />
-                        <a href="">Listen</a>
+                        <a href="/listen">Listen</a>
                     </div>
                     <div id="login">
                         <img src="https://icons.iconarchive.com/icons/github/octicons/72/dot-fill-24-icon.png" alt="" />
-                        <a href="">LiveTV</a>
+                        <a href="/livetv">LiveTV</a>
                     </div>
                     <div id="login">
                         <a href=""><img src="https://icons.iconarchive.com/icons/iconoir-team/iconoir/72/search-icon.png" alt="" /></a>
                     </div>
                     <div id="login">
-                        <a href="">Sign in</a>
+                        <a href="/login">Sign in</a>
                     </div>
                     
                 </div>
