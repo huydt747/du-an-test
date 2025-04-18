92982c74b0fefaf0655b9c4ba568ecb6a7c1edeb
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-04-18 08:25:08 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default {
+export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
+    allowedHosts: ['du-an-test-1mta.onrender.com'],
   }
-}
+})
