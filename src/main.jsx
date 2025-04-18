ede1dfedffeb0caefd14151e45c66e6975cf4e2f
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-04-18 08:31:26 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css'
-import App from './App'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+import App from './App';
 import TrangChu from "./pages/TrangChu";
 import America from "./pages/America";
 import Health from "./pages/Health";
@@ -10,50 +10,22 @@ import Us from "./pages/Us";
 import Business from "./pages/Business";
 import World from "./pages/World";
 import Signin from "./pages/signin";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "@fortawesome/fontawesome-free/css/all.min.css";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <div>Lỗi 404</div>,
-    children: [
-      {
-        path:"/",
-        element: <TrangChu/>,
-      },
-      {
-        path:"/sign-in",
-        element: <Signin/>,
-      },
-      {
-        path:"/america",
-        element: <America/>,
-      },
-      {
-        path:"/health",
-        element: <Health/>,
-      },
-      {
-        path:"/us",
-        element: <Us/>,
-      },
-      {
-        path:"/business",
-        element: <Business/>,
-      },
-      {
-        path:"/world",
-        element: <World/>,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(  
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<TrangChu />} />
+          <Route path="sign-in" element={<Signin />} />
+          <Route path="america" element={<America />} />
+          <Route path="health" element={<Health />} />
+          <Route path="us" element={<Us />} />
+          <Route path="business" element={<Business />} />
+          <Route path="world" element={<World />} />
+          <Route path="*" element={<div>Lỗi 404</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
