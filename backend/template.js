65c803a07108363591e995a1db617d09f2aec658
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-05-01 14:14:49 +0700

    delete

--- backend/template.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getClient } = require('./db');
-
-router.get('/api/example', async (req, res) => {
-	const client = getClient();
-});
-
-module.exports = router;
